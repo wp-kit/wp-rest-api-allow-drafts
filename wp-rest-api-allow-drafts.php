056f1dd65d52667fc1bd7518c7d6130be5564fc9
terence1990 <terence@creativelittledots.co.uk>
2020-06-02 16:47:38 +0100

    readme spell checl

--- wp-rest-api-allow-drafts.php
+++ wp-rest-api-allow-drafts.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WP Rest API Allow Drafts
  * Version: 1.0.0
- * Plugin URI: https://wpmailsmtp.com/
+ * Plugin URI: https://github.com/wp-kit/wp-rest-api-allow-drafts
  * Description: Allows draft posts and pages to come through into WP Rest Api when requesting for single draft posts and pages by id when the referrer host name matches the home url
  * Author: Creative Little Dots & WPKit
  */
